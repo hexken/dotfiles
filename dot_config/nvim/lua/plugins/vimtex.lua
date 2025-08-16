75a74d4513bd2d5a56b2f56a22ab4d59b9545a58
Ken Tjhia <KenTjhia@gmail.com>
2025-08-16 15:29:19 -0600

    Update nvim configs - use Zathura for vimtex vimtex preview - use Ruff, standard type checking for basedpyright

--- dot_config/nvim/lua/plugins/vimtex.lua
+++ dot_config/nvim/lua/plugins/vimtex.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
     vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
-    vim.g.vimtex_view_method = "sioyek"
+    vim.g.vimtex_view_method = "zathura"
     vim.g.vimtex_compiler_latexmk = {
       aux_dir = "./.latexmk/aux",
       out_dir = "./.latexmk/out",
