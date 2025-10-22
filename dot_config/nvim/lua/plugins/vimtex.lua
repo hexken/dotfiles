4f1d9884459c2f1e79bb495413aa8a2094f7747b
Ken Tjhia <KenTjhia@gmail.com>
2025-10-21 19:23:11 -0600

    update lazyvim copilot chat, redshift changes, upcase home dirs

--- dot_config/nvim/lua/plugins/vimtex.lua
+++ dot_config/nvim/lua/plugins/vimtex.lua
@@ -5,10 +5,19 @@ return {
     vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
     vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
     vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_biblatex_biber = 1
+    vim.g.vimtex_compiler_method = "latexmk"
     vim.g.vimtex_compiler_latexmk = {
+       options = {
+         '-pdf',
+         '-verbose',
+         '-file-line-error',
+         '-synctex=1',
+         '-interaction=nonstopmode',
+       },
       aux_dir = "./.latexmk/aux",
       out_dir = "./.latexmk/out",
-    }
+      }
   end,
   keys = {
     { "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
